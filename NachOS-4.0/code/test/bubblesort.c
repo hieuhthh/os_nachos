b88e1c980769d0b009813a0262fd309465d487dc
Nhan Gia Khâm <nhangiakham@gmail.com>
2022-10-07 10:26:27 +0000

    fixed the conflicts

--- NachOS-4.0/code/test/bubblesort.c
+++ NachOS-4.0/code/test/bubblesort.c
@@ -74,26 +74,4 @@ int main()
     bubbleSort(a, n, type);
     printArray(a, n);
     PrintChar('\n');
-
-int main()
-{
-    int n, a[MAX_SIZE], i;
-
-    PrintString("Enter the number of elements n (1 <= n <= 100): ");
-    n = ReadNum();
-    for (i = 1; i <= n; ++i) {
-        PrintString("Enter the a[");
-        PrintNum(i);
-        PrintString("]: ");
-        a[i] = ReadNum();    
-    }
-    for (i = 1; i <= n; ++i){
-        PrintString("The value of a[");
-        PrintNum(i);
-        PrintString("]: ");
-        PrintNum(a[i]);
-        PrintString("\n");
-    }
-    
-    Halt();
 }
